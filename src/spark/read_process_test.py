613daa1813a563846dd0529f00c7b00c5fdbacf9
Ubuntu <ubuntu@ip-10-0-0-11.ec2.internal>
2019-06-23 23:36:58 +0000

    format

--- src/spark/read_process_test.py
+++ src/spark/read_process_test.py
@@ -28,9 +28,11 @@ def main(sc):
 	"""Read submission and comment json file from s3"""
 	submission = SparkSession(sc).read.json("s3a://insightredditdata/redditsubmission/RS_2014-01")
         comment = SparkSession(sc).read.json("s3a://insightredditdata/redditcomment/RC_2014-01")
+	
 	"""Convert unix timestamp to human readable time"""
 	time_format(submission)
 	time_format(comment)
+	
 	"""Extract author, subreddit from submission and remove duplicates"""
 	authors= submission[['author']] \
                     .union(comment[['author']]) \
@@ -38,24 +40,29 @@ def main(sc):
 	subreddit = submission[['subreddit', 'subreddit_id']] \
                        .union(comment[['subreddit', 'subreddit_id']]) \
                        .dropDuplicates())
+	
 	"""Extract submission and comment"""
 	selectS =['author','created_utc','name','num_comments','score','subreddit_id','title']
 	submission_w = submission[selectS]
 	selectC =['author','body','created_utc','link_id','name','parent_id','score','subreddit_id']
 	comment_w = comment[selectC]
+	
 	"""Clean column names"""
 	submission_w = submission_w.withColumnRenamed("name","submission_id") \
                                     .withColumnRenamed("created_utc","time")
 	comment_w = comment_w.withColumnRenamed("name","comment_id") \
                               .withColumnRenamed("link_id","submission_id") \
                                 .withColumnRenamed("created_utc","time")
+	
 	"""Generate unique id for author"""
 	authors = authors.withColumn("author_id", f.monotonically_increasing_id())
+	
 	"""Attach author_id to submisson and comment"""
 	submission_w = submission_w.join(authors,submission_w.author == authors.author,"inner") \
                                   .drop("author")
 	comment_w = comment_w.join(authors,comment_w.author == authors.author,"inner") \
                                 .drop("author")
+	
 	"""Write dataframe to redshift"""
 	write_dataframe(submission_w)
 	write_dataframe(comment_w)
