2263749608186c42a89a35449ee70fb6acbd2d5a
xuzifan08 <zx712@nyu.edu>
2019-07-02 21:07:10 +0000

    optimize airflow scheduling

--- src/airflow/schedule.py
+++ src/airflow/schedule.py
@@ -14,9 +14,15 @@ default_args = {
 }
 dag = DAG('ssh_tutorial', default_args=default_args, schedule_interval=timedelta(days=1))
 
-SPARK_CMD = """
-    spark-submit --jars $SPARK_HOME/jars/aws-java-sdk-1.7.4.jar,$SPARK_HOME/jars/RedshiftJDBC42-no-awssdk-1.2.27.1051.jar,$SPARK_HOME/jars/spark-redshift_2.10-3.0.0-preview1.jar,$SPARK_HOME/jars/spark-avro_2.11-4.0.0.jar --master spark://{master_ip}:7077 --num-executors {num_executors} {spark_filepath}
-""".format(master_ip=master_ip, num_executors=num_executors, spark_filepath=spark_filepath)
+SPARK_CMD = """spark-submit --jars \
+	$SPARK_HOME/jars/aws-java-sdk-1.7.4.jar,\
+        $SPARK_HOME/jars/RedshiftJDBC42-no-awssdk-1.2.27.1051.jar,\
+        $SPARK_HOME/jars/spark-redshift_2.10-3.0.0-preview1.jar,\
+        $SPARK_HOME/jars/spark-avro_2.11-4.0.0.jar \
+        --master spark://{master_ip}:7077 \
+        --num-executors {num_executors} \
+        {spark_filepath}"""\
+        .format(master_ip=master_ip, num_executors=num_executors, spark_filepath=spark_filepath)
 
 
 t1 = SSHOperator(task_id='Download_data_to_s3', ssh_conn_id="ssh_spark_master", command="python3 /home/ubuntu/downloaddata.py", dag=dag)
